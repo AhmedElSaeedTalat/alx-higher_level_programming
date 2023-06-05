01643a327380fbd704966b370bf8bd8dcc90c321
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-06-05 16:00:07 +0000

    task10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+  * check_cycle - checks if there is a cycle
+  *
+  * @list: linked list passed to the function
+  *
+  * Return: 1 if there is a cycle 0 if there isn't
+  */
+int check_cycle(listint_t *list)
+{
+	listint_t *ptr, *ptr2;
+
+	ptr = ptr2 = list;
+	while (ptr2 != NULL)
+	{
+		ptr2 = ptr2->next->next;
+		ptr = ptr->next;
+		if (ptr2 == ptr)
+			return (1);
+	}
+	return (0);
+}
