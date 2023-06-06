669fea74fec48ee5e6577ab1c223e1071a0185bf
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-06-06 17:38:02 +0000

    edit case null

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
-    if str is None:
-        print('\n')
-    else:
-        for index, letter in enumerate(str):
-            letter = ord(letter)
-            if letter >= 97 and letter <= 122:
-                letter = letter - 32
-            if index == len(str) - 1:
-                print("{}".format(chr(letter)))
-                continue
-            print("{}".format(chr(letter)), end='')
+    for index, letter in enumerate(str):
+        letter = ord(letter)
+        if letter >= 97 and letter <= 122:
+            letter = letter - 32
+        print("{}".format(chr(letter)), end='')
+    print('\n')
