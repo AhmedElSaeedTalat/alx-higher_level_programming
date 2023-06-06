f662a87a551ada5a82581dea3af38b160be2b028
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-06-06 14:32:12 +0000

    edit task1 for negative numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 import random
+import math
 number = random.randint(-10000, 10000)
-sign = ''
-if number < 0:
-    sign = '-'
-number = abs(number)
-if (number % 10) > 5:
+if (math.fmod(number, 10)) > 5:
     print(f"Last digit of {number:d} is {number % 10:d} and is greater than 5")
-elif (number % 10) < 6 and (number % 10) != 0:
-    print(f"Last digit of {sign}{number:d} is {sign}{number % 10:d} and\
+elif (math.fmod(number, 10)) < 6 and (math.fmod(number, 10)) != 0:
+    print(f"Last digit of {number:d} is {int(math.fmod(number, 10)):d} and\
  is less than 6 and not 0")
 else:
     print(f"Last digit of {number:d} is {number % 10:d} and is 0")
