cd8793c917ac1a0981d8081252167972870381ba
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-06-06 16:14:55 +0000

    task6 edit .format use

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(0, 10):
     for y in range(i + 1, 10):
         if y == 9 and i == 8:
-            print(f"{i}{y}")
+            print("{}{}".format(i, y))
             continue
-        print(f"{i}{y}, ", end='')
+        print("{}{}, ".format(i, y), end='')
