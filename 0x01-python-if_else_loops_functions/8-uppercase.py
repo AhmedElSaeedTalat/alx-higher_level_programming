2b56c1947aad587d3d280775bc15f4903b746237
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-06-06 17:23:43 +0000

    task8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for index, letter in enumerate(str):
+        letter = ord(letter)
+        if letter >= 97 and letter <= 122:
+            letter = letter - 32
+        if index == len(str) - 1:
+            print("{}".format(chr(letter)))
+            continue
+        print("{}".format(chr(letter)), end='')
