7dc115357edf6860eb7c73af20a3ea3e8c63884c
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-06-08 09:07:39 +0000

    task1

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    print("10 + 5 = {}".format(add(10, 5)))
+    print("10 - 5 = {}".format(sub(10, 5)))
+    print("10 * 5 = {}".format(mul(10, 5)))
+    print("10 / 5 = {}".format(div(10, 5)))
