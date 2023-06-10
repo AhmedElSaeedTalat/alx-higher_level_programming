68f116bd90d3bea0bc24cc9ac6210422f9b3a844
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-06-10 23:28:06 +0000

    t13

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+  * is_palindrome - checks if is palindrome
+  *
+  * @head: head of linked list
+  * Return: Always 0 (Success)
+  */
+int is_palindrome(listint_t **head)
+{
+	listint_t *curr;
+	int list[1024], y, x, i = 0;
+
+	if (*head == NULL)
+		return (1);
+	curr = *head;
+	while (curr != NULL)
+	{
+		list[i] = curr->n;
+		curr = curr->next;
+		i++;
+	}
+	for (y = 0, x = i - 1; y < i; y++, x--)
+	{
+		if (y == (i / 2) - 1)
+			break;
+		if (list[y] != list[x])
+			return (0);
+	}
+	return (1);
+}
