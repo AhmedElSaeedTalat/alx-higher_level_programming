89aceda350bb79651554ea7da5291629bc261b91
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-06-14 14:42:23 +0000

    t0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -5,5 +5,5 @@ def square_matrix_simple(matrix=[]):
     new_matrix = [[None] * len(matrix) for i in range(len(matrix))]
     for index, i in enumerate(matrix):
         for index2, y in enumerate(i):
-            new_matrix[index][index2] = y * y
+            new_matrix[index][index2] = y ** 2
     return new_matrix
