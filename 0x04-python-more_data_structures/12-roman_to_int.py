27a6214314c4e194e2b2503a354e2d472233b9d3
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-06-14 21:24:59 +0000

    t12 different approach

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,14 +2,14 @@
 def roman_to_int(roman_string):
     roman_d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     numerical_value = 0
+    temp = 0
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
     for index, ch in enumerate(roman_string):
-        for key, value in roman_d.items():
-            if ch == key:
-                if index == 0 and len(roman_string) == 2:
-                    if ch == 'I' or ch == 'X' or ch == 'C':
-                        numerical_value = -value
-                        continue
-                numerical_value = numerical_value + value
+        value = roman_d.get(ch)
+        if value > temp and index > 0:
+            numerical_value = value - numerical_value
+        else:
+            numerical_value = numerical_value + value
+        temp = value
     return numerical_value
