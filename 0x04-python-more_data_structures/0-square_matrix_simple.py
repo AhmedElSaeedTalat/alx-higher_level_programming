6d0bd9ac3272cde16cc015ccbe65cd07a5f49de1
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-06-14 14:37:39 +0000

    t0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    if len(matrix) == 0:
+        return None
     new_matrix = [[None] * len(matrix) for i in range(len(matrix))]
     for index, i in enumerate(matrix):
         for index2, y in enumerate(i):
