e7c80bc6c6bd545332e5eac2987d65edf281f50d
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-06-26 16:39:59 +0000

    t3

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    result = 0
+    try:
+        if a == 0 or b == 0:
+            result = None
+        else:
+            result = a / b
+    except Exception as e:
+        print(e, end='')
+    finally:
+        print("Inside result: {}".format(result))
+    return result
