31703e6d2fc9e64a564da168e2e28dc5b657807f
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-06-26 20:43:16 +0000

    t101

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    result = 0
+    try:
+        result = fct(args[0], args[1])
+    except Exception as e:
+        sys.stderr.write("Exception: {}\n".format(str(e)))
+        return None
+    else:
+        return result
