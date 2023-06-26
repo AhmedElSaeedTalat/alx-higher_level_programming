a0cd9b5c843d4503d169cd7b332fafb20d341a3e
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-06-26 15:00:44 +0000

    t2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    i = 0
+    counter = 0
+    try:
+        while (i < x):
+            if not isinstance(my_list[i], int):
+                i = i + 1
+                continue
+            print("{:d}".format(my_list[i]), end='')
+            i = i + 1
+            counter = counter + 1
+    except Exception as e:
+        print(e, end='')
+    print()
+    return counter
