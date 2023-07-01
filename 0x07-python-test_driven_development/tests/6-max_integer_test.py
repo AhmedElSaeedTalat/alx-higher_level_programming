f1cac0b0e3baae5af3ef3c5fa63623f3cb4f5849
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-07-01 18:33:29 +0000

    test cases for max_integer function

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" unit test class """
+""" Unittest for max_integer([..]) """
 
 
 import unittest
@@ -7,10 +7,37 @@ max_integer = __import__("6-max_integer").max_integer
 
 
 class TestMax(unittest.TestCase):
-    def test_max_integer(self):
+    def test_max_integer_ordered(self):
         """
-            def test_max_integer(self): test function to test max_integer
-            function
+            def test_max_integer_ordered(self): test function to
+            test max_integer function for an ordered list
         """
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def test_max_integer_empty(self):
+        """
+            def test_max_integer_empty(self): test function to test max_integer
+            function for an empty list
+        """
         self.assertEqual(max_integer([]), None)
+
+    def test_max_integer_singleValue(self):
+        """
+            def test_max_integer_singleValue(self): test function to
+            test max_integer function for a single value
+        """
+        self.assertEqual(max_integer([5]), 5)
+
+    def test_max_integer_repeatedValue(self):
+        """
+            def test_max_integer_repeatedValye(self): test function to test
+            max_integer function a repeated value in a list
+        """
+        self.assertEqual(max_integer([1, 2, 9, 9, 8, 1]), 9)
+
+    def test_max_integer_negative(self):
+        """
+            def test_max_integer_negative(self): test function to test
+            max_integer function with a list with negative values
+        """
+        self.assertEqual(max_integer([-8, -2, -9]), -2)
