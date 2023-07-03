39ac906bfbebb668a448947be53edb0b8c4f2459
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-07-03 20:42:18 +0000

    t101

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -4,7 +4,5 @@ import numpy as np
 
 def lazy_matrix_mul(m_a, m_b):
     """ def lazy_matrix_mul(m_a, m_b): mul 2 matrices """
-    l1 = np.array(m_a)
-    l2 = np.array(m_b)
-    l3 = np.dot(l1, l2)
+    l3 = np.dot(m_a, m_b)
     return l3
