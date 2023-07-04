9e75f45842058a76e9ac7e2e9b082928eb46bda1
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-07-04 20:37:30 +0000

    t101

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def magic_string():
+    counter = 0
+    counter +=1
+    print(counter)
+    return "BestSchool"
---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" class with loced attribute """
+
+
+class LockedClass:
+    """class to lock attribute"""
+
+    def __init__(self):
+        pass
+
+    def __setattr__(self, attribute, value):
+        """ __setattr__(self, attribute, value): to set attributes"""
+        if attribute != "first_name":
+            error = "'LockedClass' object has no attribute"
+            raise AttributeError(f"{error} '{attribute}'")
+        else:
+            super().__setattr__(attribute, value)
