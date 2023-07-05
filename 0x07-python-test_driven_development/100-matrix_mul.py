3caa099f4fe1accd7b99f0eb985aa42cd43be45c
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-07-05 15:17:09 +0000

    matrix add documentation task 6

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+""" function to multiply matrices """
+
+
 def matrix_mul(m_a, m_b):
+    """
+        def matrix_mul(m_a, m_b): function to multiply matrices
+    """
     if not isinstance(m_a, list):
         raise TypeError("m_a must be a list")
     if not isinstance(m_b, list):
