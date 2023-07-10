cf78434f698c7212a32d19d51e3841ebea18b007
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-07-10 15:26:29 +0000

    task 6 raise expection

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" an empty class BaseGeometry """
+
+
+class BaseGeometry:
+    """ an empty class BaseGeometry """
+    def area(self):
+        """ def area(self): raises exceptopn """
+        raise Exception("area() is not implemented")
