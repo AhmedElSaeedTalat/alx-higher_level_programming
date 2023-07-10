c59f55f888ec79de74407ef96768362e1fa3fcf5
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-07-10 21:08:13 +0000

    small edit

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -6,7 +6,7 @@ class MyInt(int):
     """ class MyInt inherits from int """
     def __eq__(self, other):
         """ __eq__(self, other): to revert behavior """
-        return not super().__eq__(other)
+        return super().__ne__(other)
 
     def __ne__(self, other):
         """ __ne__(self, other): to revert behavior """
