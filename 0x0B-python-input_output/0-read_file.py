f623ed4d30f090ee586dfbcbfc56b5fc7b1fd63f
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-07-11 12:14:49 +0000

    t0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" function to read from a file """
+
+
+def read_file(filename=""):
+    """ read_file(filename=''): to read from a file"""
+    with open(filename, "r", encoding="UTF8") as f:
+        print(f.read())
