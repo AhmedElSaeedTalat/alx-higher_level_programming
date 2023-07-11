8563bff73e407517fa3f53717118ebff023821ae
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-07-11 20:37:21 +0000

    check condition

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -9,7 +9,10 @@ def append_after(filename="", search_string="", new_string=""):
         lines = f.readlines()
     for i, line in enumerate(lines):
         if search_string in line:
-            lines.insert(i + 1, new_string)
+            if i + 1 < len(lines):
+                lines.insert(i + 1, new_string)
+            else:
+                lines.append(new_string)
     with open(filename, "w", encoding="utf8") as f:
         for line in lines:
             f.write(line)
