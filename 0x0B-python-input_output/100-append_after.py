851593da0a8eec2cbaba6463377d21a922647cbf
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-07-11 20:22:30 +0000

    task 13

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" inster text after a strting """
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """ inster text after a strting """
+    line = []
+    with open(filename, "r", encoding="utf8") as f:
+        lines = f.readlines()
+    for i, line in enumerate(lines):
+        if search_string in line:
+            lines.insert(i + 1, new_string)
+    with open(filename, "w") as f:
+        for line in lines:
+            f.write(line)
---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" returns a list of lists of integers representing the
+Pascal’s triangle """
+
+
+def pascal_triangle(n):
+    """ pascal_triangle(n):returns Pascal's triangle of n"""
+    `
