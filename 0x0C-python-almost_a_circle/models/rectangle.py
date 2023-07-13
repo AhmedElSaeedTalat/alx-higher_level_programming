7a26d79d19fd4e283ba1dbeb82a8e7ccc630f9e9
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-07-13 17:09:42 +0000

    editing private attribute

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -41,7 +41,7 @@ class Rectangle(Base):
         self.__width = width
         self.__height = height
         self.__x = x
-        self.y = y
+        self.__y = y
 
     @property
     def width(self):
