3bbef1d30facd42a26b15c91df6518e9c21490b8
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-07-15 11:54:06 +0000

    json to file task

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -20,5 +20,17 @@ class Base:
     def to_json_string(list_dictionaries):
         """to_json_string(list_dictionaries): return json representation"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
-            return []
+            return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ """
+        if list_objs is None:
+            list_objs = []
+        dicts = []
+        for obj in list_objs:
+            dicts.append(obj.to_dictionary())
+        json_string = cls.to_json_string(dicts)
+        with open(cls.__name__ + ".json", "w") as f:
+            f.write(json_string)
