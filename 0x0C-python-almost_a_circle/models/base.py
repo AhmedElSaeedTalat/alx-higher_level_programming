444ae3ea94a6669a0976f6f6f2fc5b69136ebffe
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-07-15 12:10:53 +0000

    from json string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -38,7 +38,7 @@ class Base:
     @staticmethod
     def from_json_string(json_string):
         """ from_jsont_string(json_string):"""
-        if json_string is None:
+        if json_string is None or not json_string:
             return []
         else:
             return json.loads(json_string)
