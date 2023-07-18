9cf485ce5b653453310c2d1d066a3a598b02aac4
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-07-18 15:13:47 +0000

    test load from no file1

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -135,10 +135,28 @@ class TestBase(unittest.TestCase):
         expected = '[Rectangle] (2) 0/0 - 2/4'
         self.assertEqual(list_rectangles_output[1].__str__(), expected)
 
+        s1 = Square(5)
+        s2 = Square(7, 9, 1)
+        list_squares_input = [s1, s2]
+
+        Square.save_to_file(list_squares_input)
+
+        list_squares_output = Square.load_from_file()
+
+        expected = '[Square] (5) 0/0 - 5'
+        self.assertEqual(list_squares_output[0].__str__(), expected)
+
+        expected = '[Square] (6) 9/1 - 7'
+        self.assertEqual(list_squares_output[1].__str__(), expected)
+
     def test_load_no_file(self):
         """ no file passed"""
         os.remove('Rectangle.json')
         list_output = Rectangle.load_from_file()
+        self.assertEqual(list_output, [])
+
+        os.remove('Square.json')
+        list_output = Square.load_from_file()
         self.assertEqual(list_output, [])
 
     def test_csv_functions(self):
