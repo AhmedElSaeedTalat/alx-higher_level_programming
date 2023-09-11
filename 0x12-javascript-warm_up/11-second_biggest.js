7810b05de71169734a9f33fa06973d061e07ab23
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-09-11 19:01:55 +0000

    t11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,11 +5,19 @@ if (process.argv.length <= 3) {
   console.log(0);
 } else {
   val = parseInt(process.argv[2]);
-  val2 = parseInt(process.argv[2]);
   for (let i = 2; i < process.argv.length; i++) {
     if (parseInt(process.argv[i]) > val) {
-      val2 = val;
       val = process.argv[i];
+    }
+  }
+  if (val !== process.argv[2]) {
+    val2 = process.argv[2];
+  } else {
+    val2 = process.argv[3];
+  }
+  for (let i = 2; i < process.argv.length; i++) {
+    if (parseInt(process.argv[i]) > val2 && process.argv[i] !== val) {
+      val2 = process.argv[i];
     }
   }
   console.log(val2);
