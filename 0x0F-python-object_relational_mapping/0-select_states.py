0524f6fb8e915383dcde9a3677e469df609baa90
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-09-14 20:17:27 +0000

    t0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,14 +4,15 @@
 
 import sys
 import MySQLdb
-usr = sys.argv[1]
-pas = sys.argv[2]
-my_db = sys.argv[3]
-my_host = 'localhost'
-db = MySQLdb.connect(host=my_host, user=usr, passwd=pas, db=my_db, port=3306)
-cur = db.cursor()
-cur.execute("SELECT * FROM states ORDER BY id ASC")
-rows = cur.fetchall()
-for row in rows:
-    info_tuple = tuple(str(col) for col in row)
-    print(info_tuple)
+if __name__ == "__main__":
+    usr = sys.argv[1]
+    ps = sys.argv[2]
+    my_db = sys.argv[3]
+    myHost = 'localhost'
+    db = MySQLdb.connect(host=myHost, user=usr, passwd=ps, db=my_db, port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        info_tuple = tuple(str(col) for col in row)
+        print(info_tuple)
