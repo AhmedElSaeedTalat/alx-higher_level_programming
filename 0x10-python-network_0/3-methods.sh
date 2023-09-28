072d2625d37d49ef6992e7a7594254a5528e621a
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-09-28 19:53:41 +0000

    t3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #script that takes in a URL and displays all HTTP methods the server will accept
-curl -s -i -X OPTIONS "$1" | awk '/Allow:/ {for (i=2; i<=NF;i++) printf "%s ", $i}';echo ""
+curl -s -i -X OPTIONS "$1" | awk '/Allow:/ {for (i=2; i<=NF;i++) printf "%s ", $i}';printf "\n"
