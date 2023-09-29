d47c78333e756257aff9de5f2dffe96900071dd6
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-09-29 20:04:57 +0000

    status code

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" sends a POST request to the passed URL with the email as a parameter"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    res = requests.get(url)
+    print(res.status_code)
