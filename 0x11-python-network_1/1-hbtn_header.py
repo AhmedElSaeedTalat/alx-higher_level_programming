b80aaa55b1628c2bd9256fdbe0f4309d931d8c4f
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-09-29 15:47:30 +0000

    task 1 display x_request_id

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """ displays the value of the X-Request-Id """
-from urllib.request import urlopen
+import urllib.request
 import sys
 
 
 url = sys.argv[1]
-with urlopen(url) as f:
+with urllib.request.urlopen(url) as f:
     headers = f.getheaders()
     for header in headers:
         if header[0] == 'X-Request-Id':
