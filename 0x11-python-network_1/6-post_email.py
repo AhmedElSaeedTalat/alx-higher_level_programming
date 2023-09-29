b736608621df644fdbf50e5c50d509c9ed0e68cb
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-09-29 19:45:50 +0000

    t6 post request with requests module

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,6 +7,6 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-    data = {'email': email}
+    data = {"email": email}
     res = requests.post(url, data=data)
     print(res.text)
