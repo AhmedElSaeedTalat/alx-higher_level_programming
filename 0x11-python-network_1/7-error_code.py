5a61d7a85d37a35a6ff9eae2ea3395c32be0f716
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-09-29 20:13:56 +0000

    status code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -2,9 +2,13 @@
 """ get a status code """
 import requests
 import sys
+from requests.exceptions import HTTPError
 
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    res = requests.get(url)
-    print('Error code:', res.status_code)
+    try:
+        res = requests.get(url)
+        print(res.text)
+    except HTTPError as e:
+        print('Error code:', res.status_code)
