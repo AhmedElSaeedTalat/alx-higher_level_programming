8d81f558347dd25769cee00723a1815aefbddbbc
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-10-01 23:19:23 +0000

    t6 peak number

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,24 @@
+#!usr/bin/python
+""" find peak number """
+
+
+def find_peak(list_of_integers):
+    if list_of_integers is None or len(list_of_integers) == 0:
+        return None
+    lst = list_of_integers
+    for i in range(0, len(lst)):
+        if i == 0:
+            if lst[i] > lst[i + 1]:
+                peak = lst[i]
+                return peak
+        elif i != 0 and i != len(lst) - 1:
+            j = i - 1
+            y = i + 1
+            if lst[i] > lst[j] and lst[i] > lst[y]:
+                peak = lst[i]
+                return peak
+        elif i == len(lst) - 1:
+            if lst[i] > lst[i - 1]:
+                peak = lst[i]
+                return peak
+    return lst[0]
