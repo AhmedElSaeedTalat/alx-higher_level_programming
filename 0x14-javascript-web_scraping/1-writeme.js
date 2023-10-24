f1b8dd16c28d95f790440a6391eac33bf78ed224
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-10-24 14:03:42 +0000

    task 1 write in a file fs

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 const fs = require('fs');
-const file_path = process.argv[2];
+const filePath = process.argv[2];
 const string = process.argv[3];
-fs.writeFile(file_path, string, function (error) {
+fs.writeFile(filePath, string, function (error) {
   if (error) {
     console.log(error);
-    return;
   }
 });
