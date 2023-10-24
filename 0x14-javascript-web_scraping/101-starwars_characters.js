8446f69d703fe5e839281d170d77c6b57975b894
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-10-24 22:18:43 +0000

    task 101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,16 +1,21 @@
 #!/usr/bin/node
 const request = require('request');
-request('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], function (error, response, body) {
+request('https://swapi-api.alx-tools.com/api/films/', function (error, response, body) {
   if (error) {
     console.log(error);
   }
   const data = JSON.parse(body);
-  for (let i = 0; i < data.characters.length; i++) {
-    request(data.characters[i], function (error, response, body) {
-      if (error) {
-        console.log(error);
+  for (let i = 0; i < data.results.length; i++) {
+    if (i === Number(process.argv[2]) - 1) {
+      for (let y = 0; y < data.results[i].characters.length; y++) {
+        request(data.results[i].characters[y], function (error, response, body) {
+          if (error) {
+            console.log(error);
+          }
+          console.log(JSON.parse(body).name);
+        });
       }
-      console.log(JSON.parse(body).name);
-    });
+      break;
+    }
   }
 });
