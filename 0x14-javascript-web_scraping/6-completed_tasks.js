3f14e698a48a17b903ae1f2918a1c336191b6790
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-10-24 21:12:49 +0000

    task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -5,16 +5,13 @@ request(process.argv[2], function (error, response, body) {
     console.log(error);
   }
   const data = JSON.parse(body);
-  let counter = 0;
   const dict = {};
-  for (let i = 1; i <= data[data.length - 1].userId; i++) {
-    for (let y = 0; y < data.length; y++) {
-      if (data[y].userId === i && data[y].completed === true) {
-        counter += 1;
-      }
+  for (let i = 0; i < data.length; i++) {
+    if (dict[data[i].userId] && data[i].completed === true) {
+      dict[data[i].userId] += 1;
+    } else if (!dict[data[i].userId] && data[i].completed === true) {
+      dict[data[i].userId] = 1;
     }
-    dict[String(i)] = counter;
-    counter = 0;
   }
   console.log(dict);
 });
